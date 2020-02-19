6c43894e6cb48b85526244b5acb2836f6a80d5a0
Chalmiller <chalmiller1@gmail.com>
2020-02-18 20:07:39 -0800

    Apollo GraphQL Schema

--- schema.js
+++ schema.js
@@ -3,21 +3,57 @@ const axios = require('axios');
 const { GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLBoolean, GraphQLList, GraphQLSchema } = require('graphql');
 
 // Primary Data Query
-const GiphyResponseType = new GraphQLObjectType({
-    name: 'Data',
+const DataResponseType = new GraphQLObjectType({
+    name: "Data",
     fields: () => ({
-        data: {
-            type: new GraphQLList(GifDataType)
-        }
+        data: {type: new GraphQLList(SearchResponseType)},
+        pagination: {type: PaginationType},
+        meta: {type: MetaType}
+    })
+})
+
+const SearchResponseType = new GraphQLObjectType({
+    name: 'Search',
+    fields: () => ({
+        id: {type: GraphQLString},
+        title: {type: GraphQLString},
+        url: {type: GraphQLString},
+        embed_url: {type: GraphQLString}, 
     })
 });
 
-// GifDataType
-const GifDataType = new GraphQLObjectType({
-    name: 'GifInfo',
+const PaginationType = new GraphQLObjectType({
+    name: 'Pagination',
     fields: () => ({
-        id: {
-            type: GraphQLString
-        }
+        total_count: {type: GraphQLInt},
+        count: {type: GraphQLInt},
+        offset: {type: GraphQLInt}
     })
 })
+
+const MetaType = new GraphQLObjectType({
+    name: 'Meta',
+    fields: () => ({
+        status: {type: GraphQLInt},
+        msg: {type: GraphQLString},
+        response_id: {type: GraphQLString}
+    })
+})
+
+// Root Query 
+const RootQuery = new GraphQLObjectType({
+    name: "RootQueryType",
+    fields: {
+        data: {
+            type: DataResponseType,
+            resolve(parent, args) {
+                return axios.get("https://api.giphy.com/v1/gifs/search?api_key=t4HfXGHIt2uMBoaMk25K5LToS1pLu7E8&q=Synths&limit=2&offset=0&rating=G&lang=en")
+                            .then(res => res.data);
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});
